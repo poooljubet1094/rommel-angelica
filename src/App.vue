8540850205bc4cd087b315d6458fe2962bb5bf03
Paul Calibuso <poooljubet@gmail.com>
2025-05-21 19:21:15 +0800

    adding background music

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,37 @@
 <script setup lang="ts">
 import NavMenu from './components/NavMenu.vue'
+import { ref, onMounted } from 'vue'
+
+const audioPlayer = ref<HTMLAudioElement | null>(null)
+
+const playAudio = () => {
+  if (audioPlayer.value) {
+    audioPlayer.value.play()
+  }
+}
+
+onMounted(() => {
+  setInterval(() => playAudio(), 3000);
+})
+
 </script>
 
 <template>
   <div class="w-full h-screen">
     <NavMenu></NavMenu>
     <div class="w-full flex justify-center">
-      <div class="p-4 max-w-4xl w-full">
+      <div class="px-4 py-8 max-w-4xl w-full">
         <RouterView></RouterView>
       </div>
+    </div>
+    <div class="w-full flex justify-center items-center mb-8 pb-8">
+      <audio ref="audioPlayer" controls autoplay loop>
+        <source src="./assets/background-sound.mp3" type="audio/mpeg">
+        Your browser does not support the audio element.
+      </audio>
     </div>
   </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
